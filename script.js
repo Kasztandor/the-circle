740b39891697b6cadd4e392c873994fa9d6573ae
Kasztandor <michal.swoboda@interia.pl>
2022-04-10 22:47:38 +0200

    some changes, added nextLevel(), etc.

--- script.js
+++ script.js
@@ -55,80 +55,102 @@ function display(){
 function checkHitboxes(x=playerCoords[1], y=playerCoords[0]){
 	var xx = Math.floor((x+bs/2)/bs)
 	var yy = Math.floor((y+bs/2)/bs)
+	//console.log(xx+" "+yy)
 	//var surroundings = [level.map[yy-1][xx-1]+level.map[yy-1][xx]+level.map[yy-1][xx+1],level.map[yy][xx-1]+level.map[yy][xx]+level.map[yy][xx+1],level.map[yy+1][xx-1]+level.map[yy+1][xx]+level.map[yy+1][xx+1]]
 	var hitted = []
-	for (var i=0; i<3; i++){
-		for (var j=0; j<3; j++){
-			switch (i+""+j){
-				case "00":
-					if (false){
-						hitted.indexOf("top")==-1?hitted.push("top"):0
-						hitted.indexOf("left")==-1?hitted.push("left"):0
-					}
-					break
-				case "01":
-					if (obstacles.indexOf(level.map[yy-1][xx])!=-1 && yy*bs>=y){
-						hitted.indexOf("top")==-1?hitted.push("top"):0
-					}
-					if (level.map[yy-1][xx] == "Z" && yy*bs>=y){
-						hitted.push("zipline")
-					}
-					break
-				case "02":
-					if (false){
-						hitted.indexOf("top")==-1?hitted.push("top"):0
-						hitted.indexOf("right")==-1?hitted.push("right"):0
-					}
-					break
-				case "10":
-					if (obstacles.indexOf(level.map[yy][xx-1])!=-1 && xx*bs>=x){
-						hitted.indexOf("left")==-1?hitted.push("left"):0
-					}
-					break
-				case "12":
-					if (obstacles.indexOf(level.map[yy][xx+1])!=-1 && xx*bs<=x){
-						hitted.indexOf("right")==-1?hitted.push("right"):0
-					}
-					break
-				case "20":
-					if (false){
-						hitted.indexOf("down")==-1?hitted.push("down"):0
-						hitted.indexOf("left")==-1?hitted.push("left"):0
-					}
-					break
-				case "21":
-					if (obstacles.indexOf(level.map[yy+1][xx])!=-1 && yy*bs<=y){
-						hitted.indexOf("down")==-1?hitted.push("down"):0
-					}
-					break
-				case "22":
-					if (false){
-						hitted.indexOf("down")==-1?hitted.push("down"):0
-						hitted.indexOf("right")==-1?hitted.push("right"):0
-					}
-					break
+	/* WIN */
+	if (level.map[yy][xx] == "W"){
+		hitted.push("WIN")
+	}
+	else if (level.map[yy][xx] == "D"){
+		hitted.push("LOSE")
+	}
+	else{
+		/* indise */
+		if (obstacles.indexOf(level.map[yy][xx])!=-1){
+			hitted.push("inside")
+		}
+		/* ⬁ */
+		if (false){
+			hitted.indexOf("top")==-1?hitted.push("top"):0
+			hitted.indexOf("left")==-1?hitted.push("left"):0
+		}
+		/* ⇧ */
+		if (obstacles.indexOf(level.map[yy-1][xx])!=-1 && yy*bs>=y){
+			hitted.indexOf("top")==-1?hitted.push("top"):0
+			if (level.map[yy-1][xx] == "Z" && yy*bs>=y){
+				hitted.push("zipline")
 			}
+		}
+		/* ⬀ */
+		if (false){
+			hitted.indexOf("top")==-1?hitted.push("top"):0
+			hitted.indexOf("right")==-1?hitted.push("right"):0
+		}
+		/* ⇦ */
+		if (obstacles.indexOf(level.map[yy][xx-1])!=-1 && xx*bs>=x){
+			hitted.indexOf("left")==-1?hitted.push("left"):0
+		}
+		/* ⇨ */
+		if (obstacles.indexOf(level.map[yy][xx+1])!=-1 && xx*bs<=x){
+			hitted.indexOf("right")==-1?hitted.push("right"):0
+		}
+		/* ⬃ */
+		if ((yy+1)<level.map.length && false){
+			hitted.indexOf("down")==-1?hitted.push("down"):0
+			hitted.indexOf("left")==-1?hitted.push("left"):0
+		}
+		/* ⇩ */
+		if ((yy+1)<level.map.length && obstacles.indexOf(level.map[yy+1][xx])!=-1 && yy*bs<=y){
+			hitted.indexOf("down")==-1?hitted.push("down"):0
+		}
+		/* ⬂ */
+		if ((yy+1)<level.map.length && false){
+			hitted.indexOf("down")==-1?hitted.push("down"):0
+			hitted.indexOf("right")==-1?hitted.push("right"):0
 		}
 	}
 	return hitted
 	//console.log(surroundings[0]+"\n"+surroundings[1]+"\n"+surroundings[2])
 }
 function gameLoop(){
-	var allowJump = false
-	if (pressedButtons.indexOf("ArrowUp") !== -1 || pressedButtons.indexOf("KeyW") !== -1){
-		if (checkHitboxes().indexOf("down")!=-1){
-			jumpVector = 15
-			allowJump = true
-		}
+	/* WIN */
+	if (checkHitboxes().indexOf("WIN")!=-1){
+		nextLevel()
 	}
-	if (checkHitboxes().indexOf("down")!=-1 && allowJump==false){
-		jumpVector=0
+	else if (checkHitboxes().indexOf("LOSE")!=-1){
+		runLevel()
 	}
 	else{
-		playerCoords[0]-=jumpVector
-		jumpVector-=1
-		if (checkHitboxes().indexOf("down")!=-1){
-			while (checkHitboxes().indexOf("down")!=-1){
+		/* check jump */
+		var allowJump = false
+		if (pressedButtons.indexOf("ArrowUp") !== -1 || pressedButtons.indexOf("KeyW") !== -1){
+			if (checkHitboxes().indexOf("down")!=-1){
+				jumpVector = 15
+				allowJump = true
+			}
+		}
+		if (pressedButtons.indexOf("ArrowLeft") !== -1 || pressedButtons.indexOf("KeyA") !== -1){
+			if (checkHitboxes().indexOf("left")==-1){
+				playerCoords[1] -= 10
+			}
+		}
+		else if (pressedButtons.indexOf("ArrowRight") !== -1 || pressedButtons.indexOf("KeyD") !== -1){
+			if (checkHitboxes().indexOf("right")==-1){
+				playerCoords[1] += 10
+			}
+		}
+		/* make jump and calculate jump vector */
+		if (checkHitboxes().indexOf("down")!=-1 && allowJump==false){
+			jumpVector=0
+		}
+		else{
+			playerCoords[0]-=jumpVector
+			jumpVector<=35?jumpVector-=1:0
+		}
+		/* repair inside block situations */
+		if (checkHitboxes().indexOf("down")!=-1 || checkHitboxes().indexOf("inside")!=-1){
+			while (checkHitboxes().indexOf("down")!=-1 || checkHitboxes().indexOf("inside")!=-1){
 				playerCoords[0]--
 			}
 			playerCoords[0]++
@@ -143,21 +165,12 @@ function gameLoop(){
 			playerCoords[0]--
 		}
 	}
-	if (pressedButtons.indexOf("ArrowLeft") !== -1 || pressedButtons.indexOf("KeyA") !== -1){
-		if (checkHitboxes().indexOf("left")==-1){
-			playerCoords[1] -= 10
-		}
-	}
-	else if (pressedButtons.indexOf("ArrowRight") !== -1 || pressedButtons.indexOf("KeyD") !== -1){
-		if (checkHitboxes().indexOf("right")==-1){
-			playerCoords[1] += 10
-		}
-	}
 	//console.log("X:"+playerCoords[1]+" Y: "+playerCoords[0])
 	//console.log(h)
 	display()
 }
-function runLevel(a){
+function runLevel(a=level.id){
+	clearInterval(gameLoopInterval)
 	loadLevel(a)
 	setTimeout(()=>{
 		for (var i = 0; i<level.map.length; i++){
@@ -171,12 +184,14 @@ function runLevel(a){
 		gameLoopInterval = setInterval(gameLoop, 33)
 	},1)
 }
+function nextLevel(){
+	runLevel(level.id+1)
+}
 function keyChecker(a, b){
 	if (b){
 		if (pressedButtons.indexOf(a) == -1){
 			pressedButtons.push(a)
 		}
-		
 	}
 	else{
 		var index = pressedButtons.indexOf(a)
@@ -191,5 +206,5 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', (e) => {
 	keyChecker(e.code, false)
 });
-runLevel(3)
+runLevel(1)
 setTimeout(display,1)
